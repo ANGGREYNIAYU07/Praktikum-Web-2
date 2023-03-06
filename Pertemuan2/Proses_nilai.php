54940d8490184abd3b0840cb10bb11e985b11f56
ANGGREYNIAYU07 <anggreyniayu7@gmail.com>
2023-03-06 22:28:17 +0700

    Proses_nilai

--- Pertemuan2/Proses_nilai.php
+++ Pertemuan2/Proses_nilai.php
@@ -5,6 +5,7 @@ if (isset($_GET['submit'])) {
     $UTS = $_GET['UTS'];
     $UAS = $_GET['UAS'];
     $Praktikum = $_GET['Praktikum'];
+    $average = ($UTS + $UAS + $Praktikum)/3;
 
     echo "Nama: $Nama <br>";
     echo "Mata Kuliah: $Matkul <br>";
@@ -12,7 +13,24 @@ if (isset($_GET['submit'])) {
     echo "Nilai UAS: $UAS <br>";
     echo "Nilai Praktikum: $Praktikum <br>";
 
+    echo "<br><br>";
 
+    echo "Nilai Rata-rata:" .round($average, 2). "<br>";
+    echo "Grade Nilai : ";
+    if ($average < 35) {
+        echo "E";
+    } elseif ($average < 55) {
+        echo "D";
+    } elseif ($average < 69) {
+        echo "C";
+    } elseif ($average < 84) {
+        echo "B";
+    } elseif ($average < 100) {
+        echo "D";
+    } else {
+        echo "I";
+    }
 } else {
-    echo "Tidak ada data!";
+    echo "Tidak Ada Data!";
 }
+?>
