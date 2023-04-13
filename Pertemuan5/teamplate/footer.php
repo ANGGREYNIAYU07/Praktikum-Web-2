a0020787a0a05c3ec20b367dcc2c66ba8f04d6a1
ANGGREYNIAYU07 <anggreyniayu7@gmail.com>
2023-04-14 00:07:57 +0700

    tugas P5 dan P6

---
+++ Pertemuan5/teamplate/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Anggreyni Ayu Hapsary TI 07 &copy; @anggreyniayu7</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+    </div>
+</footer>
